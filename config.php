b7f9465bbc74fd6c27cc37b23081d4eb76e70b8c
1k3l9bwzacry0rig7x21xcfd4hw8fa <john@ezlife.tw>
2024-07-07 21:00:09 +0800

    database connection character set setting

--- config.php
+++ config.php
@@ -11,3 +11,9 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("連線失敗: " . $conn->connect_error);
 }
+
+// 設定連線字元集為 utf8mb4
+$conn->set_charset('utf8mb4');
+
+// 建立資料庫查詢時,也要指定使用 utf8mb4 字元集
+$conn->query("SET NAMES 'utf8mb4'");
